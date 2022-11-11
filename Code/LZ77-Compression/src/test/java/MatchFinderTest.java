f16408b23ee07f18f350598d16a2d31e037ea0ec
Quicano <tobiasvoelkner@hotmail.com>
2022-11-11 20:33:39 +0100

    Signed-off-by: Quicano <tobiasvoelkner@hotmail.com>

--- Code/LZ77-Compression/src/test/java/MatchFinderTest.java
+++ Code/LZ77-Compression/src/test/java/MatchFinderTest.java
@@ -22,4 +22,18 @@ public class MatchFinderTest {
         String decodedString = decoder.decode(list);
         System.out.println(decodedString);
     }
+
+    @Test
+    public void testRandomString(){
+        String s = "Hier Testet ihr Hier hier der String";
+        MatchFinder matchFinder = new MatchFinder();
+        Dictionary d = matchFinder.lookForMatch(s);
+        ArrayList<Entry> list = d.getDictionary();
+        for (Entry e : list){
+            System.out.println(e.getOffset() +","+e.getLength()+","+ e.getNextSymbol());
+        }
+        Decoder decoder = new Decoder();
+        String decodedString = decoder.decode(list);
+        System.out.println(decodedString);
+    }
 }
